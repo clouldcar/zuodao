e76dfd94b2878f1e4431887a1ad1604b4ef7d65c
wanghaowei123 <phphw123@163.com>
2018-07-29 10:15:24 +0800

    用户的mode

--- common/config/bootstrap.php
+++ common/config/bootstrap.php
@@ -3,6 +3,4 @@ Yii::setAlias('@common', dirname(__DIR__));
 Yii::setAlias('@frontend', dirname(dirname(__DIR__)) . '/frontend');
 Yii::setAlias('@backend', dirname(dirname(__DIR__)) . '/backend');
 Yii::setAlias('@console', dirname(dirname(__DIR__)) . '/console');
-Yii::setAlias('@platform', dirname(dirname(__DIR__)) . '/platform');
-Yii::setAlias('@api', dirname(dirname(__DIR__)) . '/api');
 Yii::setAlias('@shop', dirname(dirname(__DIR__)) . '/shop');
