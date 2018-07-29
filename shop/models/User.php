16c037aa0b1c6f04a5e805fc1031c87846226795
wanghaowei123 <phphw123@163.com>
2018-07-29 11:00:48 +0800

    用户的查询

--- shop/models/User.php
+++ shop/models/User.php
@@ -21,4 +21,29 @@ class User extends \common\models\User
     {
         return Yii::$app->security->validatePassword($password, $this->password);
     }
+
+    /*
+     * @name 获取user信息
+     * @param $param 查询条件 $select 查询字段
+     * @return array();
+     */
+    public function userList($param,$select){
+        return User::find()->select($select)->where($param)->asArray()->all();
+    }
+
+    /*
+     * @name 想user表插入数据
+     * @param value
+     * @return str
+     */
+    public function addUser($param){
+        $data = Yii::$app->db->createCommand()->insert(self::tableName(), $param)->execute();
+        if($data){
+            return $user_id = Yii::$app->db->getLastInsertID();
+        }else{
+            return '';
+        }
+    }
+
+
 }
