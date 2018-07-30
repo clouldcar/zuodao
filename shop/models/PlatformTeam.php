1424245a0336de9ca9bcdf144f4da4ab5cfddbe0
wanghaowei123 <phphw123@163.com>
2018-07-30 22:31:52 +0800

    写了下团队相关 包括团队创建编辑展示 团队成员的添加删除展示编辑

---
+++ shop/models/PlatformTeam.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace shop\models;
+
+use yii;
+
+/*
+ * @name 平台下的团队model
+ */
+
+class PlatformTeam extends  \yii\db\ActiveRecord
+{
+    public static function tableName()
+    {
+        return '{{%platform_team}}';
+    }
+
+    /*
+     * @name 通过平台id 查看平台下的团队列表
+     */
+    public function teamList($platformId){
+        $teamList =  $this->find()->select('name,public,status,start_date,create_time')->where(['platform_id'=>$platformId])->asArray()->all();
+        return $teamList?$teamList:[];
+    }
+
+    /*
+     * @name 添加新的团队
+     */
+    public function teamCreate($param){
+        $data = Yii::$app->db->createCommand()->insert(self::tableName(), $param)->execute();
+        if($data){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+
+
+    /*
+     * @name 编辑团队
+     */
+    public function teamEditor($param){
+        $data = Yii::$app->db->createCommand()->update(self::tableName(), $param)->execute();
+        if($data){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+
+
+
+
+
+}
---
+++ shop/models/TeamUser.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace shop\models;
+
+use yii;
+
+/*
+ * @name 用户团队关联Model
+ */
+
+class TeamUser extends  \yii\db\ActiveRecord
+{
+    const  ADD_BATCH  = 1;
+    const  ADD_ALONE  = 0;
+
+    public static function tableName()
+    {
+        return '{{%team_user}}';
+    }
+
+    /*
+     * @name 增加成员
+     */
+    public function addMembers($data){
+        $result = '';
+        if($data['type'] == self::ADD_BATCH){
+            //批量添加
+            unset($data['type']);
+            $result = Yii::$app->db->createCommand()->batchInsert(self::tableName(), $data)->execute();
+
+        }
+
+        if($data['type'] == self::ADD_ALONE){
+            //单独添加
+            unset($data['type']);
+            $result = Yii::$app->db->createCommand()->insert(self::tableName(), $data)->execute();
+        }
+
+        if(!$result){
+            return false;
+        }
+        return true;
+    }
+
+    /*
+    * @name 增加成员
+    */
+    public function deleteMembers($data){
+        $result = Yii::$app->db->createCommand()->update(self::tableName(), $data)->execute();
+        if(!$result){
+            return false;
+        }
+        return true;
+    }
+
+    /*
+     * @name 修改团队成员权限
+     */
+
+    public function editorMembers($data){
+        $result = Yii::$app->db->createCommand()->update(self::tableName(), $data)->execute();
+        if(!$result){
+            return false;
+        }
+        return true;
+    }
+
+    /*
+     * @name 展示团队下的成员信息
+     */
+    public function membersList($teamId){
+        $list = $this->find()
+            ->from('shop_team_user as t')
+            ->leftJoin('shop_user as u','u.id=t.user_id')
+            ->select('t.permissions,p.create_time,u.username,u.phone')
+            ->where(['t.team_id'=>$teamId])
+            ->asArray()
+            ->all();
+        return $list?$list:[];
+
+    }
+
+
+
+}
