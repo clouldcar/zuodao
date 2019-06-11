fcb92946f0ac51cbdb11e41a732c531160a6c68d
webn716@gmail.com <webn716@gmail.com>
2019-06-11 19:49:19 +0800

    老P;

---
+++ api/controllers/GraduateController.php
@@ -0,0 +1,20 @@
+<?php
+namespace api\controllers;
+
+use Yii;
+use common\helpers\Utils;
+use api\models\Article;
+use api\models\ArticleCategory;
+
+/**
+* 老P专区
+*/
+
+class GraduateController extends BaseController
+{
+
+    public function actionIndex()
+    {
+        
+    }
+}
