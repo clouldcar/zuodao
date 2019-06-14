a3702a4dac096864558ad84533db740bdc4442c2
webn716@gmail.com <webn716@gmail.com>
2019-06-14 18:03:21 +0800

    老P

--- api/controllers/GraduateController.php
+++ api/controllers/GraduateController.php
@@ -13,8 +13,39 @@ use api\models\ArticleCategory;
 class GraduateController extends BaseController
 {
 
+    public function actionGlobal()
+    {
+        parent::checkGet();
+        $data = Yii::$app->request->get();
+        $uid = Yii::$app->user->id;
+        $type = ArticleCategory::TYPE_ID_GRAD;
+
+        $category = ArticleCategory::getCategoriesByType($type, 1, 10);
+
+        $result[
+            'categories' => $category['list']
+        ];
+
+        return Utils::returnMsg(0, null, $result);
+    }
+
     public function actionIndex()
     {
-        
+        parent::checkGet();
+        $data = Yii::$app->request->get();
+        $uid = Yii::$app->user->id;
+
+        $page = isset($data['page']) ? $data['page'] : 1;
+        if ($page < 1) {
+            $page = 1;
+        }
+        $page_size = 20;
+
+        if (!isset($data['cid']) || !$data['cid']) {
+            return Utils::returnMsg(1, '缺少必要参数');
+        }
+        $list = Article::getListByCId($data['cid'], $page, $page_size);
+
+        return Utils::returnMsg(0, null, $list);
     }
 }
