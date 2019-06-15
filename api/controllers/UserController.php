00dcc12f9de586eb338c90aaf591e125575a047e
webn716@gmail.com <webn716@gmail.com>
2019-06-15 21:57:43 +0800

    登录

--- api/controllers/UserController.php
+++ api/controllers/UserController.php
@@ -92,7 +92,7 @@ class UserController extends BaseController
 
             //设置session
             // $model->setSession($model->_user->id);
-            return Utils::returnMsg(0, "登录成功", empty(Yii::$app->user->identity->updated_at));
+            return Utils::returnMsg(0, "登录成功", ['status' => empty(Yii::$app->user->identity->updated_at)]);
         } else {
             return Utils::returnMsg(1, "登录失败");
         }
