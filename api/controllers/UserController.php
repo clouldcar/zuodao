d022637a33ffd233fc8f81a87d1c979c42bf40dd
webn716@gmail.com <webn716@gmail.com>
2019-06-15 21:36:48 +0800

    登录

--- api/controllers/UserController.php
+++ api/controllers/UserController.php
@@ -92,7 +92,7 @@ class UserController extends BaseController
 
             //设置session
             // $model->setSession($model->_user->id);
-            return Utils::returnMsg(0, "登录成功", empty($model->updated_at));
+            return Utils::returnMsg(0, "登录成功", empty($model->attributes['updated_at']));
         } else {
             return Utils::returnMsg(1, "登录失败");
         }
@@ -201,7 +201,7 @@ class UserController extends BaseController
         Yii::$app->session->removeAll();
         Yii::$app->session->destroy();
 
-        return Utils::returnMsg(1, "登出成功");
+        return Utils::returnMsg(0, "登出成功");
     }
 
     public function actionSmsCode()
