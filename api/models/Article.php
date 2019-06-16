5b8f2313ddf9ee8101b30159cbfade87a35b4ee7
webn716@gmail.com <webn716@gmail.com>
2019-06-16 13:35:10 +0800

    运营推荐文章

--- api/models/Article.php
+++ api/models/Article.php
@@ -146,7 +146,8 @@ class Article extends \yii\db\ActiveRecord {
             ->all();
 
         foreach ($list as &$item) {
-            $item['desc'] = substr(strip_tags($item['content']), 0, 260);
+            $item['desc'] = Utils::truncateUtf8String(strip_tags($item['content']), 260);
+            unset($item['content']);
             // $item['desc'] = '移动小号手机卡0月租卡无线流量卡电话卡视频卡号码卡4g卡上网卡,中国移动4G无线上网卡24G包年卡路由器随身mifi纯流量卡全国电信';
             $item['user'] = UserInfo::getInfoByUID($item['uid'], 1);
         }
